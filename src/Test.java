5c9de9d689faef5e11307a729e27900356227ad8
Karagoez <aka160@post.uit.no>
2020-01-16 13:09:27 +0100

    new files

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,3 @@
 public class Test {
-  public static Object max(Object o1, Object o2) {
-	Object o3 = (Comparable)o1;
-	Object o4 = (Comparable)o2;
-    if ((Comparable)o1.compareTo((Comparable)o2) >= 0) {
-      return o1;
-    } else {
-      return o2;
-    }
-  }
-  public static void main(String args[]) {
-	  Object o1 = new Object();
-	  Object o2 = new Object();
-	  
-	  max(o1, o2);
-  }
+	
 }
--- src/TilnaermetPhi.java
+++ src/TilnaermetPhi.java
@@ -1,4 +1,3 @@
-
 public class TilnaermetPhi {
 
 	public static void main(String[] args) {
