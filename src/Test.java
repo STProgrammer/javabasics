4860903bbd3f5f9518f641e66d89e9b32d9ff5d1
Karagoez <aka160@post.uit.no>
2020-01-26 23:45:34 +0100

    new files

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,11 @@
+import java.io.*;
+
 public class Test {
-	
+	public static void main(String args[]) throws IOException {
+		DataOutputStream output = new DataOutputStream(
+				new FileOutputStream("d.dat"));
+		output.writeUTF("ABCD");
+		System.out.println(output.size());
+		output.close();
+	}
 }
